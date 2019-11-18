f83983c7ddc55c4b7a6a94fe2702ed889d3b7380
mandiesitian <185175485@qq.com>
2019-11-18 14:56:32 +0800

    注解

--- IOC/src/main/java/model/UserBean.java
+++ IOC/src/main/java/model/UserBean.java
@@ -2,12 +2,17 @@ package model;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component(value = "user")
 public class UserBean implements BeanNameAware, BeanFactoryAware, InitializingBean, DisposableBean {
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println(beanFactory);
     }
 
+    @Value("123")
     public void setBeanName(String s) {
         System.out.println(s);
     }
